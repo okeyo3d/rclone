367bfcbd95c5d812df19b8391fc293b5be5cc5ad
okeyo3d <164302553+okeyo3d@users.noreply.github.com>
2025-02-05 22:06:03 +0300

    Update copy.sh - v2

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rclone sync okeyo3d:/ archive:okeyo3d@gmail.com/My\ Drive/ --progress --drive-acknowledge-abuse --min-age 401d
+rclone move okeyo3d:/ archive:okeyo3d@gmail.com/My\ Drive/ --progress --drive-trashed-only --drive-acknowledge-abuse --min-age 401d
